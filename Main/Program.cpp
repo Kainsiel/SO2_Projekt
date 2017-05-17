3143c0fd8ca9175823e28eee12123c50f77cfc17
Kainsiel <m4lesniak@gmail.com>
2017-05-18 00:52:15 +0200

    1# Reading from file

--- Main/Program.cpp
+++ Main/Program.cpp
@@ -1,13 +1,19 @@
 #include <thread>
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 
 using namespace std; 
 
 //zwraca macierz zczytana z pliku
 void wczytaj_macierz()
 {
-
+	ifstream plik;
+	plik.open("tekst.txt", fstream::in);
+	if (!plik.is_open()){
+	  throw "Brak pliku z danymi";
+	}
+	plik.close();
 }
 //tworzy macierz z wylosowanymi wartosciami
 void losuj_macierz()
@@ -16,6 +22,8 @@ void losuj_macierz()
 }
 
 //transparuj macierz
+void transparu_macierz(){
+}
 
 int main()
 {
