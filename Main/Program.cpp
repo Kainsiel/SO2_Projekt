c7957950ca3cd5600a86bf4995974ee3ee23bfc4
Kainsiel <m4lesniak@gmail.com>
2017-05-18 03:13:23 +0200

    2# Reading from file

--- Main/Program.cpp
+++ Main/Program.cpp
@@ -5,16 +5,58 @@
 
 using namespace std; 
 
-//zwraca macierz zczytana z pliku
-void wczytaj_macierz()
+int wczytaj_rozmiar()
 {
+	int rozmiar;
+	
 	ifstream plik;
 	plik.open("tekst.txt", fstream::in);
 	if (!plik.is_open()){
 	  throw "Brak pliku z danymi";
 	}
+
+	plik >> rozmiar;
+	
 	plik.close();
+	return rozmiar;
 }
+
+//zwraca macierz zczytana z pliku
+int** wczytaj_macierz()
+{
+	int rozmiar;
+	int** macierz;
+	
+	
+
+	ifstream plik;
+	plik.open("tekst.txt", fstream::in);
+	if (!plik.is_open()){
+	  throw "Brak pliku z danymi";
+	}
+	plik >> rozmiar;
+	
+	macierz = new int*[rozmiar];
+	for(int i = 0; i < rozmiar; i++)
+		macierz[i] = new int[rozmiar];
+
+	for(int i = 0; i < rozmiar; i++){
+	for (int j= 0; j < rozmiar; j++){
+	 plik >> macierz[i][j]; 
+	}	
+	}
+	
+	for(int i = 0; i < rozmiar; i++){
+	for (int j= 0; j < rozmiar; j++){
+	 cout << macierz[i][j] << " "; 	
+	}
+	 cout << "\n";
+	}
+
+	plik.close();
+	return macierz;	
+}
+
 //tworzy macierz z wylosowanymi wartosciami
 void losuj_macierz()
 {
@@ -25,8 +67,8 @@ void losuj_macierz()
 void transparu_macierz(){
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-
+  wczytaj_macierz();
   return 0;
 }
