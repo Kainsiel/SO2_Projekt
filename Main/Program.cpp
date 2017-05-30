36c5bd0835d50dc7e10158cd49a42ca10eed93f1
Kainsiel <m4lesniak@gmail.com>
2017-05-30 23:26:27 +0200

    Different Approach

--- Main/Program.cpp
+++ Main/Program.cpp
@@ -2,141 +2,134 @@
 #include <mutex>
 #include <iostream>
 #include <stdlib.h>
-#include <fstream>
 #include <time.h>
 #include <vector>
+#include <ncurses.h>
+#include <chrono>
+#include <condition_variable>
 
-using namespace std; 
+using namespace std;
 
-int const m = 2;
-mutex mtx[m][m];
-thread watek[m];
+int **A;
+int const M = 5;
+int const N = 5;
+vector<int> kolejka1;
+mutex mx;
+vector<int> kolejka2;
+int spr = 0;
+condition_variable cv;
 
 
-//zwraca macierz zczytana z pliku
-int** wczytaj_macierz()
-{
-	int rozmiar = m;
-	int** macierz;
-	
-	
-
-	ifstream plik;
-	plik.open("tekst.txt", fstream::in);
-	if (!plik.is_open()){
-	  throw "Brak pliku z danymi";
-	}
-	
-	macierz = new int*[rozmiar];
-	for(int i = 0; i < rozmiar; i++)
-		macierz[i] = new int[rozmiar];
-
-	for(int i = 0; i < rozmiar; i++){
-		for (int j= 0; j < rozmiar; j++){
-		 plik >> macierz[i][j]; 
-		}	
-	}
-	
-	for(int i = 0; i < rozmiar; i++){
-		for (int j= 0; j < rozmiar; j++){
-		 cout << macierz[i][j] << " "; 	
-		}
-	 cout << "\n";
-	}
-
-	plik.close();
-	return macierz;	
+int getY(int size, int pos){
+	int rows = pos/size;
+	return rows;
 }
 
-//tworzy macierz z wylosowanymi wartosciami
-int** losuj_macierz()
+int getX(int size, int pos){
+	int rows = pos/size;
+	return pos-rows*size;
+}
+
+void Producent(){
+
+unique_lock<mutex> lock(mx);
+
+kolejka1.push_back(rand() % M);
+kolejka2.push_back(rand() % N);
+
+cv.wait(lock,[]{return spr == 1;});
+
+}
+
+void Przetwarzacz()
 {
-  int** macierz;
-  int rozmiar = m;
+  int var1 , var2;
+  lock_guard<mutex> lock(mx);
+  var1 = kolejka1.back();
+  printw(" %d ", var1);
+  kolejka1.pop_back();
+  var2 = kolejka2.back();
+  printw(" %d \n", var2);
+  kolejka2.pop_back();
 
-  macierz = new int*[rozmiar];
-  for(int i = 0; i < rozmiar; i++)
-    macierz[i] = new int[rozmiar];
+  int temp = A[M-1][N-1];
+  for (int i=M*N-1; i>0; i--){
+  A[getY(M, i)][getX(N, i)] = A[getY(M, i-1)][getX(N, i-1)];
+  }
+  A[0][0] = temp;
 
-  for(int i = 0; i < rozmiar; i++){
-  for (int j= 0; j < rozmiar; j++){
-    macierz[i][j] = rand() % 100; 
-  }	
-  }	
+
+   spr=1;
+  cv.notify_one();
+}
+
+void Wyswietl()
+{
+
+   for(int i = 0; i < M; i++){
+	for (int j= 0; j < N; j++){
+	  printw(" %d ", A[i][j]);
+	}
+     printw("\n");
+}
+   
   
-  cout << "\n";
-  for(int i = 0; i < rozmiar; i++){
-	for (int j= 0; j < rozmiar; j++){
-	 cout << macierz[i][j] << " "; 	
-	}
-	 cout << "\n";
-	}
-  cout << "\n";
-  return macierz;
-}
-
-void pomnoz_macierz(int const * const * A, int const * const * B,int** C,int row,int col, int i){
-    mtx[row][col].lock();
-    C[row][col] += A[row][i]*B[i][col];
-    mtx[row][col].unlock();
+   
+  
 }
 
 //transparuj macierz
-void transparuj_macierz(){
-}
 
-int main(int argc, char *argv[])
+int main()
 {
   srand(time(NULL));
- int **A,**B,**C;
- int i,j,k,s;
+  initscr();
+  vector<thread> threads;
+  vector<thread> threadp;
+  vector<thread> threadw;
 
-
-
-
- A = wczytaj_macierz();
- B = losuj_macierz();
-
-
-
- C = new int * [m];
- for(int i = 0; i < m; i++)
+ A = new int * [M];
+ for(int i = 0; i < M; i++)
  {
-	C[i] = new int[m];
+	A[i] = new int[N];
  }
 
-
- for(int row=0; row<m; row++){
-        for(int col=0; col<m;col++){
-            for(int i=0;i<m;i++){
-                watek[i] = thread(pomnoz_macierz, A, B, C,row, col, i);
-            }
-        }
-    }
+ for(int i = 0; i < M; i++){
+	for (int j= 0; j < N; j++){
+	  A[i][j] = 0; 	
+	}
+}
 
 
-      for(int i=0;i<2;i++){
-         watek[i].join();
-      }
+printw("\n");
+  for (int i = 0; i < 10; i++){
+    threads.push_back(thread(Producent));
+    
 
+}
 
-  for(i = 0; i < m; i++)
-  {
-    for(j = 0; j < m; j++) cout << " " << C[i][j];
-    cout << endl;
-  }
+  for (int i = 0; i < 10; i++){
+    threadp.push_back(thread(Przetwarzacz));
+    
 
+}
+
+  for (int i = 0; i < 10; i++){
+    threadw.push_back(thread(Wyswietl));
+    
+}
+
+for(auto& thread : threadw)
+  thread.join();
+
+for(auto& thread : threadp)
+  thread.join();
+
+for(auto& thread : threads)
+  thread.join();	
+
+ getch();
   
-  for(i = 0; i < m; i++)
-  {
-    delete [] A[i];
-    delete [] B[i];
-    delete [] C[i];
-  }
-
-  delete [] A;
-  delete [] B;
-  delete [] C; 
-
+  endwin();
   return 0;
 }
