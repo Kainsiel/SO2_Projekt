1ac296b53b4e16a3119209afbcde68628daf02e7
Kainsiel <m4lesniak@gmail.com>
2017-06-03 14:55:29 +0200

    Display

--- Main/Program.cpp
+++ Main/Program.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-int **A;
+
 int const M = 5;
 int const N = 5;
 queue <int> kolejka1;
@@ -18,8 +18,11 @@ mutex mx1;
 
 queue <int> kolejka2;
 mutex mx2;
-int spr = 0;
-condition_variable cv;
+mutex mx3;
+
+int A[M][N];
+mutex mtx;
+
 
 
 int getY(int size, int pos){
@@ -32,11 +35,32 @@ int getX(int size, int pos){
 	return pos-rows*size;
 }
 
+void Wyczysc_macierz(){
+
+ while(1){
+   mtx.lock();
+   for(int i=0; i<M; i++){
+	 for(int j = 0; j<N; j++){
+	   A[i][j] = 0;
+	   }    		
+   }
+   mtx.unlock();
+ }
+ 
+ this_thread::sleep_for(std::chrono::seconds(30));
+}
+
 void Producent(){
-
-kolejka1.push_back(rand() % M);
-kolejka2.push_back(rand() % N);
-
+srand(time(NULL));
+while(1){
+  mx1.lock();
+  kolejka1.push(rand() % M);
+  mx1.unlock();
+  mx2.lock();
+  kolejka2.push(rand() % N);
+  mx2.unlock();
+  this_thread::sleep_for (std::chrono::milliseconds(100)); 
+}
 
 
 }
@@ -44,73 +68,67 @@ kolejka2.push_back(rand() % N);
 void Przetwarzacz()
 {
   int var1 , var2;
-  var1 = kolejka1.back();
-  printw(" %d ", var1);
-  kolejka1.pop_back();
-  var2 = kolejka2.back();
-  printw(" %d \n", var2);
-  kolejka2.pop_back();
-
+  mx1.lock();
+  if(int spr = kolejka1.back())
+    var1 = kolejka1.back();
+  mx1.unlock();
+   
+  mx2.lock();
+  if(int spr = kolejka2.back())
+    var2 = kolejka2.back();
+  mx2.unlock();
+  mx3.lock();
   A[var1][var2]=1;
-
-  int temp = A[M-1][N-1];
+  mx3.unlock();
+/*  int temp = A[M-1][N-1];
   for (int i=M*N-1; i>0; i--){
   A[getY(M, i)][getX(N, i)] = A[getY(M, i-1)][getX(N, i-1)];
   }
   A[0][0] = temp;
-
+  
+*/
   
 }
 
 void Wyswietl(){
 
-this_thread::sleep_for(chrono::seconds(1));
+this_thread::sleep_for(chrono::milliseconds(500));
 
-
+mtx.lock();
 for(int i = 0; i < M; i++){
 	for (int j= 0; j < N; j++){
 	  printw(" %d ", A[i][j]);
 	}
      printw("\n");
 }
-   printw("\n");
+printw("\n");
+mtx.unlock();
    refresh();
 }
 
 int main()
 {
-  srand(time(NULL));
+  
   initscr();
-  vector<thread> thread_producent;
-  vector<thread> thread_przetwarzacz;
-  vector<thread> thread_wyswietl;
 
- A = new int * [M];
- for(int i = 0; i < M; i++)
- {
-	A[i] = new int[N];
- }
+auto tWyczysc = thread(Wyczysc_macierz);
+  
+auto tWyswietl = thread(Wyswietl);
 
- for(int i = 0; i < M; i++){
-	for (int j= 0; j < N; j++){
-	  A[i][j] = 0; 	
-	}
-}
+auto tProducent = thread(Producent);
+
+//auto tPrzetwarzacz = thread(Przetwarzacz);
 
 
+tWyczysc.join();
 
-  auto tProducent = thread(Producent);
+tWyswietl.join();
 
-  auto tPrzetwarzacz = thread(Przetwarzacz);
+tProducent.join();
 
-  auto tWyswietl = thread(Wyswietl);
+//tPrzetwarzacz.join();
 
 
-  tProducent.join();
-
-  tPrzetwarzacz.join();
-
-  tWyswietl.join();
 
 getch();
   
